db6a2a725dfb70a7fe4975f51f1bdb36287b5b58
AnsonL <ans0nlxdd@icloud.com>
2021-05-03 20:27:56 +0800

    Create date-parser.js

---
+++ src/server/routes/modules/date-parser.js
@@ -0,0 +1,36 @@
+/* Convert time from 12Hr format to 24Hr format
+   Original format: HH:MM(am/pm)
+   Output format: HH:MM:00 */
+function timeConvert(time) {
+    let sepPos = time.search(':');
+    let m = time.slice(-2);
+    time = time.slice(0, -2);
+    let hr = Number(time.slice(0, sepPos));
+    let min = time.slice(sepPos+1);
+
+    if (m == 'pm') {hr += 12;}
+    else if (hr < 10) {hr = '0' + String(hr);}
+    if (min.length == 1) {min = '0' + min;}
+    return `${hr}:${min}:00`;
+}
+
+/* Convert datetime to mongoose compatible format
+   Original format: dd/mm/YYYY HH:MM(am/pm)
+   Output format: YYYY-mm-ddTHH:MM:ss */
+function convertDateMongoose(dateStr) {
+    let sepPos = dateStr.search('/');
+    let d = dateStr.slice(0, sepPos);
+    dateStr = dateStr.slice(sepPos+1)
+    sepPos = dateStr.search('/');
+    let m = dateStr.slice(0, sepPos);
+    let y = dateStr.slice(sepPos+1, sepPos + 5);
+    let time = dateStr.slice(sepPos+6)
+     
+
+    if (m.length == 1) {m = '0' + m;}
+    if (d.length == 1) {d = '0' + d;}
+
+    return `${y}-${m}-${d}${timeConvert(time)}`;
+}
+
+module.exports = {timeConvert, convertDateMongoose};
