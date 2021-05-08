d0abfbff6da1943e738d7258c03576c890df47d2
Thisiss0710 <82769462+Thisiss0710@users.noreply.github.com>
2021-05-08 17:00:42 +0800

    Names Added

--- src/server/routes/modules/date-parser.js
+++ src/server/routes/modules/date-parser.js
@@ -1,3 +1,10 @@
+//---------------------------------------------------
+// Cheng Yun Chueng 1155109570
+// Wong Kong Wa 1155127049
+// Chow Wang Faat 1155115793
+// Lau Pak Hei Anson 1155158646
+//---------------------------------------------------
+
 /* Convert time from 12Hr format to 24Hr format
    Original format: HH:MM(am/pm)
    Output format: HH:MM:00 */
